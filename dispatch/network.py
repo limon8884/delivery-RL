551281d8b33ddc193edcebcb42fd005fed75d720
limon8884 <dan.limon.db@gmail.com>
2023-01-20 16:20:54 +0300

    new positional-point encoder

--- dispatch/network.py
+++ dispatch/network.py
@@ -7,59 +7,102 @@ from torch.nn.utils.rnn import pad_sequence
 from utils import *
 from dispatch.utils import *
 
-def ExtractOrderFeatures(order: Order, current_time: int) -> List[float]:
-    return [
-            order.point_from.x, 
-            order.point_from.y, 
-            order.point_to.x, 
-            order.point_to.y, 
-            order.expire_time - current_time
-    ]
+class FeatureExtractor:
+    def __init__(self) -> None:
+        self.num_features = {
+            'o': {
+                'points': 2,
+                'numbers': 1,
+            },
+            'c': {
+                'points': 1,
+                'numbers': 0,
+            },
+            'ar': {
+                'points': 1,
+                'numbers': 1,
+            },
+        }
+        
+    def __call__(self, item, current_time, item_type):
+        if item_type == 'o':
+            result = {
+                'points': [item.point_from, item.point_to], 
+                'numbers': [item.expire_time - current_time],
+            }
+            assert len(result['points']) == self.num_features['o']['points']
+            assert len(result['numbers']) == self.num_features['o']['numbers']
+        elif item_type == 'c':
+            result = {
+                'points': [item.position], 
+                'numbers': [],
+            } 
+            assert len(result['points']) == self.num_features['c']['points']
+            assert len(result['numbers']) == self.num_features['c']['numbers']
+        elif item_type == 'ar':
+            if item.eta_period:
+                dist = distance(item.courier.position, item.order.point_from)\
+                    + distance(item.order.point_from, item.order.point_to)
+            else:
+                dist = dist(item.courier.position, item.order.point_to)
 
-def ExtractCourierFeatures(courier: Courier, current_time: int=0) -> List[float]:
-    return [
-            courier.position.x, 
-            courier.position.y
-    ]
+            result = {
+                'points': [item.order.point_to], 
+                'numbers': [dist],
+            }
+            assert len(result['points']) == self.num_features['ar']['points']
+            assert len(result['numbers']) == self.num_features['ar']['numbers']
+        else: 
+             raise Exception('not found type!')
+        
+        return result
 
-def ExtractActiveRoutesFeatures(active_route: ActiveRoute, current_time: int=0) -> List[float]:
-    if active_route.eta_period:
-        dist = distance(active_route.courier.position, active_route.order.point_from)\
-            + distance(active_route.order.point_from, active_route.order.point_to)
-    else:
-        dist = dist(active_route.courier.position, active_route.order.point_to)
-    return [
-        active_route.order.point_to.x, 
-        active_route.order.point_to.y,
-        dist
-    ]
-
-class PositionalEncoder(nn.Module):
-    def __init__(self, type, pos_enc_dim, out_dim, device=None):
+class PointEncoder(nn.Module):
+    def __init__(self, point_enc_dim, device):
         super().__init__()
-        assert pos_enc_dim % 2 == 0
-        self.sin_layer = nn.Linear(1, pos_enc_dim // 2, device=device)
-        self.cos_layer = nn.Linear(1, pos_enc_dim // 2, device=device)
+        assert point_enc_dim % 4 == 0
+        self.point_enc_dim = point_enc_dim
+        self.sin_layer = nn.Linear(1, point_enc_dim // 4, device=device)
+        self.cos_layer = nn.Linear(1, point_enc_dim // 4, device=device)
+        self.device = device
+
         # nn.init.uniform_(self.sin_layer.weight, 0, 100)
         # nn.init.uniform_(self.cos_layer.weight, 0, 100)
         # self.trainable = trainable
         # self.freqs = torch.tensor([1 / 2**i for i in range(pos_enc_dim // 2)])
-        if type == 'o':
-            self.feature_extractor = ExtractOrderFeatures
-            num_features = 5
-        elif type == 'c':
-            self.feature_extractor = ExtractCourierFeatures
-            num_features = 2
-        elif type == 'ar':
-            self.feature_extractor = ExtractActiveRoutesFeatures
-            num_features = 3
-        else:
-            raise Exception('not found type!')
 
+    def forward(self, p: Point):
+        x = torch.tensor([p.x, p.y], dtype=torch.float32, device=self.device).unsqueeze(-1)
+        return torch.cat([torch.sin(self.sin_layer(x)), torch.cos(self.cos_layer(x))], dim=-1).flatten()
+    
+        # pos_enc = torch.cat([torch.sin(f * self.freqs), torch.cos(f * self.freqs)], dim=-1)
+        # if self.trainable:
+
+# class NumericEncoder(nn.Module):
+#     def __init__(self, point_enc_dim, device):
+#         super().__init__()
+#         assert point_enc_dim % 2 == 0
+#         self.sin_layer = nn.Linear(1, point_enc_dim // 2, device=device)
+#         self.cos_layer = nn.Linear(1, point_enc_dim // 2, device=device)
+#         self.device = device
+
+#     def forward(self, x: float):
+#         x = torch.tensor(x, dtype=torch.float32, device=self.device).unsqueeze(-1)
+#         return torch.cat([torch.sin(self.sin_layer(x)), torch.cos(self.cos_layer(x))], dim=-1).flatten()
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, item_type: str, point_encoder: PointEncoder, num_enc_dim, out_dim, device=None):
+        super().__init__()
+        self.feature_extractor = FeatureExtractor()
+        self.point_encoder = point_encoder
+        self.number_encoder = nn.Linear(1, num_enc_dim, device=device)
+        self.item_type = item_type
+
+        pt_enc_dim = self.point_encoder.point_enc_dim
+        input_dim = self.feature_extractor.num_features[item_type]['points'] * pt_enc_dim \
+                + self.feature_extractor.num_features[item_type]['numbers'] * num_enc_dim 
         self.mlp = nn.Sequential(
-            nn.LayerNorm(num_features * pos_enc_dim),
-            nn.LeakyReLU(),
-            nn.Linear(num_features * pos_enc_dim, out_dim),
+            nn.Linear(input_dim, out_dim),
             nn.LeakyReLU(),
             nn.Linear(out_dim, out_dim)
         )
@@ -67,21 +110,31 @@ class PositionalEncoder(nn.Module):
         self.device = device
 
     def forward(self, item, current_time):
-        x = self.feature_extractor(item, current_time)
-        x = torch.tensor(x, dtype=torch.float32, device=self.device).unsqueeze(-1)
-        # pos_enc = torch.cat([torch.sin(f * self.freqs), torch.cos(f * self.freqs)], dim=-1)
-        # if self.trainable:
-        pos_enc = torch.cat([torch.sin(self.sin_layer(x)), torch.cos(self.sin_layer(x))], dim=-1)
-    
-        return self.mlp(torch.flatten(pos_enc))
+        features = self.feature_extractor(item, current_time, self.item_type)
+        encoded_points = [self.point_encoder(p) for p in features['points']]
+        encoded_numbers = [
+                self.number_encoder(torch.tensor([n], dtype=torch.float32, device=self.device)) 
+                for n in features['numbers']
+        ]
+
+        return self.mlp(torch.cat(encoded_points + encoded_numbers))
     
 
 class ScoringNet(nn.Module):
-    def __init__(self, mode='default', n_layers=2, d_model=256, n_head=2, dim_ff=256, pos_enc_dim=64, device=None):
+    def __init__(self, 
+            mode='default', 
+            n_layers=2, 
+            d_model=256, 
+            n_head=2, 
+            dim_ff=256, 
+            point_enc_dim=32, 
+            number_enc_dim=8,
+            device=None):
         super().__init__()
-        self.order_enc = PositionalEncoder(type='o', out_dim=d_model, pos_enc_dim=pos_enc_dim, device=device)
-        self.courier_enc = PositionalEncoder(type='c', out_dim=d_model, pos_enc_dim=pos_enc_dim, device=device)
-        self.active_routes_enc = PositionalEncoder(type='ar', out_dim=d_model, pos_enc_dim=pos_enc_dim, device=device)
+        self.point_encoder = PointEncoder(point_enc_dim=point_enc_dim, device=device)
+        self.order_enc = PositionalEncoder('o', self.point_encoder, num_enc_dim=number_enc_dim, out_dim=d_model, device=device)
+        self.courier_enc = PositionalEncoder('c', self.point_encoder, num_enc_dim=number_enc_dim, out_dim=d_model, device=device)
+        self.active_routes_enc = PositionalEncoder('ar', self.point_encoder, num_enc_dim=number_enc_dim, out_dim=d_model, device=device)
         self.mode = mode
         self.device = device
 
@@ -142,9 +195,9 @@ class ScoringNet(nn.Module):
             batch_c.append(triple.couriers)
             batch_ar.append(triple.active_routes)
 
-        o_tensor, o_mask = self.make_tensor(batch_o, type='o', current_time=current_time)
-        c_tensor, c_mask = self.make_tensor(batch_c, type='c', current_time=current_time)
-        ar_tensor, ar_mask = self.make_tensor(batch_ar, type='ar', current_time=current_time)
+        o_tensor, o_mask = self.make_tensor(batch_o, item_type='o', current_time=current_time)
+        c_tensor, c_mask = self.make_tensor(batch_c, item_type='c', current_time=current_time)
+        ar_tensor, ar_mask = self.make_tensor(batch_ar, item_type='ar', current_time=current_time)
         self.masks = {
             'o': o_mask,
             'c': c_mask,
@@ -163,15 +216,15 @@ class ScoringNet(nn.Module):
     #     tens = pad_sequence(samples, batch_first=True)
     #     return tens, create_mask(lenghts)
 
-    def make_tensor(self, batch_sequences, type, current_time):
+    def make_tensor(self, batch_sequences, item_type, current_time):
         samples = []
         lenghts = []
         for sequence in batch_sequences:
-            if type == 'o':
+            if item_type == 'o':
                 sample = torch.stack([self.order_enc(item, current_time) for item in sequence])
-            elif type == 'c':
+            elif item_type == 'c':
                 sample = torch.stack([self.courier_enc(item, current_time) for item in sequence])
-            elif type == 'ar':
+            elif item_type == 'ar':
                 sample = torch.stack([self.active_routes_enc(item, current_time) for item in sequence])
             samples.append(sample)
             lenghts.append(len(sequence))
