b4a9d931d0eaa71acdcf178a475be664a525c166
limon8884 <dan.limon.db@gmail.com>
2023-06-20 12:33:47 +0300

    add BOS items, fix BOS masks

--- networks/scoring_v1.py
+++ networks/scoring_v1.py
@@ -11,6 +11,24 @@ from networks.utils import *
 from objects.gamble_triple import GambleTriple
 
 
+def unmask_BOS_items(masks):
+    masks_new = {
+        'o': masks['o'].clone().detach(),
+        'c': masks['c'].clone().detach(),
+        'ar': masks['ar'].clone().detach()
+    }
+    masks_new['o'][:, 0] = False
+    masks_new['c'][:, 0] = False
+    masks_new['ar'][:, 0] = False
+
+    return masks_new
+
+# def unmask_BOS_items(masks):
+#     masks['o'][:, 0] = False
+#     masks['c'][:, 0] = False
+#     masks['ar'][:, 0] = False
+
+
 class ScoringNet(nn.Module):
     def __init__(self, 
             mode='default', 
@@ -32,26 +50,7 @@ class ScoringNet(nn.Module):
         self.dim_ff = dim_ff
         self.n_layers = n_layers
         self.point_enc_dim = point_enc_dim
-        # self.point_encoder = point_encoder
 
-        # self.order_enc = PositionalEncoder('o', 
-        #                                        self.point_encoder, 
-        #                                        num_enc_dim=number_enc_dim, 
-        #                                        out_dim=d_model,  
-        #                                        device=device
-        #                                        )
-        # self.courier_enc = PositionalEncoder('c', 
-        #                                          self.point_encoder, 
-        #                                          num_enc_dim=number_enc_dim, 
-        #                                          out_dim=d_model, 
-        #                                          device=device
-        #                                          )
-        # self.ar_enc = PositionalEncoder('ar', 
-        #                                     self.point_encoder, 
-        #                                     num_enc_dim=number_enc_dim, 
-        #                                     out_dim=d_model, 
-        #                                     device=device
-        #                                 )     
         self.encoders_AR = nn.ModuleDict({
             'o': nn.TransformerDecoderLayer(
                 d_model=d_model, 
@@ -104,60 +103,20 @@ class ScoringNet(nn.Module):
             nn.Flatten(start_dim=-2)
         )
         
-    
     def forward(self, tensors, masks):
-        # self.masks = None
-        # ord, crr, ar = self.make_tensors_and_create_masks(batch, current_time)
+        masks = unmask_BOS_items(masks)
 
         ord, crr = self.encoder_AR(tensors['o'], tensors['c'], tensors['ar'], masks)
+        assert ord.isnan().sum() == 0 and crr.isnan().sum() == 0, 'nan values are not allowed\n' + str(ord)
         ord, crr = self.encoder_OC(ord, crr, masks)
         ord_scores, ord_fake_crr = self.score_and_fake_heads(ord)
 
         scores = self.bipartite_scores(ord, crr)
         final_scores = torch.cat([scores, ord_fake_crr], dim=-1)
+        values = torch.mean(ord_scores, dim=-1)
 
-        return final_scores, ord_scores
-
-    # def make_tensors_and_create_masks(self, batch: List[GambleTriple], current_time):
-    #     batch_c = []
-    #     batch_o = []
-    #     batch_ar = []
-    #     for triple in batch:
-    #         batch_o.append(triple.orders)
-    #         batch_c.append(triple.couriers)
-    #         batch_ar.append(triple.active_routes)
-
-    #     o_tensor, o_mask = self.make_tensor(batch_o, item_type='o', current_time=current_time)
-    #     c_tensor, c_mask = self.make_tensor(batch_c, item_type='c', current_time=current_time)
-    #     ar_tensor, ar_mask = self.make_tensor(batch_ar, item_type='ar', current_time=current_time)
-    #     self.tensors = {
-    #         'o': o_tensor,
-    #         'c': c_tensor,
-    #         'ar': ar_tensor
-    #     }
-    #     self.masks = {
-    #         'o': o_mask,
-    #         'c': c_mask,
-    #         'ar': ar_mask
-    #     }
-
-    #     return o_tensor, c_tensor, ar_tensor
-
-    # def make_tensor(self, batch_sequences, item_type, current_time):
-    #     samples = []
-    #     lenghts = []
-    #     for sequence in batch_sequences:
-    #         if item_type == 'o':
-    #             sample = torch.stack([self.order_enc(item, current_time) for item in sequence])
-    #         elif item_type == 'c':
-    #             sample = torch.stack([self.courier_enc(item, current_time) for item in sequence])
-    #         elif item_type == 'ar':
-    #             sample = torch.stack([self.ar_enc(item, current_time) for item in sequence])
-    #         samples.append(sample)
-    #         lenghts.append(len(sequence))
-        
-    #     tens = pad_sequence(samples, batch_first=True)
-    #     return tens, create_mask(lenghts, self.device)
+        # unmask_BOS_items(masks)
+        return final_scores, values
 
     def encoder_AR(self, ord, crr, ar, masks):
         new_ord = self.encoders_AR['o'](ord, ar, tgt_key_padding_mask=masks['o'], memory_key_padding_mask=masks['ar'])
@@ -182,12 +141,6 @@ class ScoringNet(nn.Module):
         ord_scores = self.ord_score_head(ord)
 
         return ord_scores, fake_head
-
-    # def get_mask(self):
-    #     with torch.no_grad():
-    #         om_ones = torch.where(self.masks['o'], 0, 1).unsqueeze(-1).float()
-    #         cm_ones = torch.where(self.masks['c'], 0, 1).unsqueeze(-2).float()
-    #         return torch.matmul(om_ones, cm_ones).float()
 
 
 class ScoringInterface:
@@ -220,6 +173,9 @@ class ScoringInterface:
         self.load_net_weights(path_weights + '/net.pt')
 
     def encode_input(self, batch: List[GambleTriple], current_time):
+        '''
+        Attention: the function adds BOS-fake items to every GambleTriple to strugle zero-input problem
+        '''
         batch_c = []
         batch_o = []
         batch_ar = []
@@ -249,35 +205,54 @@ class ScoringInterface:
         }
 
     def make_tensor(self, batch_sequences, item_type, current_time):
+        '''
+        Input: a batch of sequences of items
+        Output: an embedding tensor of shape [bs, max_len + 1, emb_size]
+        where max_len - maximum sequence length over the batch; emb_size - d_model
+        The function adds a BOS item with zeros-embedding
+        '''
         samples = []
         lenghts = []
         ids_batch = []
         for sequence in batch_sequences:
-            if len(sequence) == 0:
-                sample = torch.zeros((1, self.d_model), device=self.device, dtype=torch.float)
-                ids = torch.tensor([-1], dtype=torch.int, device=self.device)
-            elif item_type == 'o':
-                sample = torch.stack([self.order_enc(item, current_time) for item in sequence])
-                ids = torch.tensor([item.id for item in sequence], dtype=torch.int, device=self.device)
+            # if len(sequence) == 0:
+            #     sample = torch.zeros((1, self.d_model), device=self.device, dtype=torch.float)
+            #     ids = torch.tensor([-1], dtype=torch.int, device=self.device)
+            bos_tensor = torch.zeros(self.d_model, device=self.device, dtype=torch.float)
+            if item_type == 'o':
+                sample = torch.stack([bos_tensor] + [self.order_enc(item, current_time) for item in sequence])
+                ids = torch.tensor([-1] + [item.id for item in sequence], dtype=torch.int, device=self.device)
             elif item_type == 'c':
-                sample = torch.stack([self.courier_enc(item, current_time) for item in sequence])
-                ids = torch.tensor([item.id for item in sequence], dtype=torch.int, device=self.device)
+                sample = torch.stack([bos_tensor] + [self.courier_enc(item, current_time) for item in sequence])
+                ids = torch.tensor([-1] + [item.id for item in sequence], dtype=torch.int, device=self.device)
             elif item_type == 'ar':
-                sample = torch.stack([self.ar_enc(item, current_time) for item in sequence])
-                ids = torch.tensor([item.id for item in sequence], dtype=torch.int, device=self.device)
+                sample = torch.stack([bos_tensor] + [self.ar_enc(item, current_time) for item in sequence])
+                ids = torch.tensor([-1] + [item.id for item in sequence], dtype=torch.int, device=self.device)
+            else:
+                raise RuntimeError('unkown item type')
             samples.append(sample)
-            lenghts.append(len(sequence))
+            lenghts.append(len(sequence) + 1)
             ids_batch.append(ids)
         
         tens = pad_sequence(samples, batch_first=True)
         ids_batch = pad_sequence(ids_batch, batch_first=True, padding_value=-1)
-        return tens, create_mask(lenghts, self.device), ids_batch
+        return tens, create_mask(lenghts, self.device, mask_first=True), ids_batch
     
     def inference(self) -> Any:
         assert self.tensors is not None, 'call encode first'
         self.scores, self.values = self.net(self.tensors, self.masks)
     
         return self.scores   
+    
+    # def mask_BOS_items(self):
+    #     self.masks['o'][:, 0] = True
+    #     self.masks['c'][:, 0] = True
+    #     self.masks['ar'][:, 0] = True
+
+    # def unmask_BOS_items(self):
+    #     self.masks['o'][:, 0] = False
+    #     self.masks['c'][:, 0] = False
+    #     self.masks['ar'][:, 0] = False
     
     def get_assignments_batch(self, gamble_triples: List[GambleTriple]):
         assignments_batch = []
@@ -291,7 +266,7 @@ class ScoringInterface:
                 for o_idx, c_idx in enumerate(argmaxes[batch_idx].numpy()):
                     if c_idx != self.scores.shape[-1] - 1 and mask[batch_idx][o_idx][c_idx] \
                             and o_idx not in assigned_orders and c_idx not in assigned_couriers:
-                        assignment = (gamble_triples[batch_idx].orders[o_idx].id, gamble_triples[batch_idx].couriers[c_idx].id)
+                        assignment = (gamble_triples[batch_idx].orders[o_idx - 1].id, gamble_triples[batch_idx].couriers[c_idx - 1].id)
                         assignments_batch[-1].append(assignment)
                         assigned_orders.add(o_idx)
                         assigned_couriers.add(c_idx)
@@ -305,16 +280,19 @@ class ScoringInterface:
         mask = self.get_mask()
         assert len(batch_assignments) == mask.shape[0]
     
-        tgt_ass = torch.where(mask[:, :, 0] == 1, mask.shape[2], -1)
+        has_orders = mask.sum(dim=-1) > 0
+        if has_orders.sum() == 0:
+            return 0
+        
+        tgt_ass = torch.where(has_orders, mask.shape[2], -1)
         for idx, assignments in enumerate(batch_assignments):
-            # row_ids, col_ids = solver(scores)
             for row, col in assignments:
-                tgt_ass[idx][row] = col
+                tgt_ass[idx][row + 1] = col + 1
 
         mask_inf_add_fake = torch.cat([(1 - mask) * -1e9, torch.zeros((mask.shape[0], mask.shape[1], 1), device=self.device)], dim=-1)
 
         ce_loss = nn.CrossEntropyLoss(reduction='sum', ignore_index=-1)
-        loss = ce_loss((mask_inf_add_fake + self.scores).transpose(1, 2), tgt_ass) / mask[:, :, 0].sum()
+        loss = ce_loss((mask_inf_add_fake + self.scores).transpose(1, 2), tgt_ass) / has_orders.sum()
         return loss
 
     def load_encoder_weights(self, path: str):
@@ -325,7 +303,11 @@ class ScoringInterface:
     def load_net_weights(self, path: str):
         self.net.load_state_dict(torch.load(path, map_location=self.device))
 
-    def get_mask(self):
+    def get_mask(self) -> torch.FloatTensor:
+        '''
+        Returns mask of shape [bs, num_orders, num_couriers + 1] (including BOS-items)
+        1 is elements is not masked, 0 otherwise
+        '''
         with torch.no_grad():
             om_ones = torch.where(self.masks['o'], 0, 1).unsqueeze(-1).float()
             cm_ones = torch.where(self.masks['c'], 0, 1).unsqueeze(-2).float()
--- networks/utils.py
+++ networks/utils.py
@@ -1,13 +1,14 @@
 import torch
 
-def create_mask(lenghts, device)->torch.BoolTensor:
+def create_mask(lenghts, device, mask_first)->torch.BoolTensor:
     '''
     returns True if element is masked, False otherwise
     '''
     max_len = max(lenghts)
     with torch.no_grad():
-        if max_len == 0:
-            return torch.ones((len(lenghts), 1), device=device, dtype=torch.bool)
+        assert max_len > 0, 'Tensor should not be empty'
         result = torch.arange(max_len, device=device).expand(len(lenghts), max_len) >= torch.tensor(lenghts, device=device).unsqueeze(1)
+        if mask_first:
+            result[:, 0] = True
         return result
     
