444b99add67037ffbcf89d2eee464037a931e39d
limon8884 <dbokatenko@nes.ru>
2024-03-20 10:25:37 -0500

    change run_dsp -> evaluate

--- main.py
+++ main.py
@@ -16,22 +16,6 @@ from src.reinforcement.delivery import DeliveryActorCritic
 from src.evaluation import evaluate
 
 
-def run_dsp(dsp: BaseDispatch, config_path: Path, db_path: Path, run_id: int, max_num_points_in_route: int) -> None:
-    db_logger = DatabaseLogger(run_id=run_id)
-    reader = DataReader.from_config(config_path=config_path, sampler_mode='dummy_sampler', logger=db_logger)
-    route_maker = AppendRouteMaker(max_points_lenght=max_num_points_in_route, cutoff_radius=0.0)
-    sim = Simulator(data_reader=reader, route_maker=route_maker, config_path=config_path, logger=db_logger)
-    try:
-        sim.run(dsp, num_iters=288)
-    finally:
-        db = Database(db_path)
-        db.export_from_logger(db_logger)
-        # cr, ctd = db.get_metric(Metric.CR, run_id), db.get_metric(Metric.CTD, run_id)
-        print(f'CR: {db.get_metric(Metric.CR, run_id)}\n \
-            CTD: {db.get_metric(Metric.CTD, run_id)}\n \
-            \n')
-
-
 def main():
     simulator_config_path = Path('configs/simulator.json')
     network_config_path = Path('configs/network.json')
@@ -39,62 +23,70 @@ def main():
     db = Database(db_path)
     db.clear()
 
-    print('Hungarian')
-    res = evaluate(
-        dispatch=HungarianDispatch(DistanceScorer()),
-        run_id=0,
-        simulator_cfg_path='configs/simulator.json',
-        sampler_mode='distr_sampler',
-        max_num_points_in_route=8,
-        history_db_path='history.db',
-        eval_num_simulator_steps=200,
-    )
-    print(res)
+    # print('Hungarian')
+    # res = evaluate(
+    #     dispatch=HungarianDispatch(DistanceScorer()),
+    #     run_id=0,
+    #     simulator_cfg_path='configs/simulator.json',
+    #     sampler_mode='distr_sampler',
+    #     max_num_points_in_route=8,
+    #     history_db_path='history.db',
+    #     eval_num_simulator_steps=200,
+    # )
+    # print(res)
 
-    print('Greedy')
-    res = evaluate(
-        dispatch=GreedyDispatch(DistanceScorer()),
-        run_id=1,
-        simulator_cfg_path='configs/simulator.json',
-        sampler_mode='distr_sampler',
-        max_num_points_in_route=8,
-        history_db_path='history.db',
-        eval_num_simulator_steps=200,
-    )
-    print(res)
+    # print('Greedy')
+    # res = evaluate(
+    #     dispatch=GreedyDispatch(DistanceScorer()),
+    #     run_id=1,
+    #     simulator_cfg_path='configs/simulator.json',
+    #     sampler_mode='distr_sampler',
+    #     max_num_points_in_route=8,
+    #     history_db_path='history.db',
+    #     eval_num_simulator_steps=200,
+    # )
+    # print(res)
 
-    print('Greedy')
+    print('Random')
     res = evaluate(
         dispatch=RandomDispatch(),
-        run_id=1,
+        run_id=2,
         simulator_cfg_path='configs/simulator.json',
         sampler_mode='distr_sampler',
-        max_num_points_in_route=8,
+        max_num_points_in_route=4,
         history_db_path='history.db',
         eval_num_simulator_steps=200,
     )
     print(res)
 
-    # print('Neural')
-    # max_num_points_in_route = 8
-    # with open(network_config_path) as f:
-    #     net_cfg = json.load(f)['encoder']
-    # encoder = GambleEncoder(
-    #     order_embedding_dim=net_cfg['order_embedding_dim'],
-    #     claim_embedding_dim=net_cfg['claim_embedding_dim'],
-    #     courier_embedding_dim=net_cfg['courier_embedding_dim'],
-    #     route_embedding_dim=net_cfg['route_embedding_dim'],
-    #     point_embedding_dim=net_cfg['point_embedding_dim'],
-    #     number_embedding_dim=net_cfg['number_embedding_dim'],
-    #     max_num_points_in_route=max_num_points_in_route,
-    #     device=None,
-    # )
-    # ac = DeliveryActorCritic(gamble_encoder=encoder, clm_emb_size=net_cfg['claim_embedding_dim'], device=None,
-    #                          temperature=1.0)
-    # ac.load_state_dict(torch.load('checkpoints/234.pt', map_location='cpu'))
-    # run_dsp(NeuralSequantialDispatch(actor_critic=ac, max_num_points_in_route=max_num_points_in_route),
-    #         simulator_config_path, db_path, run_id=3,
-    #         max_num_points_in_route=max_num_points_in_route)
+    print('Neural')
+    max_num_points_in_route = 4
+    with open(network_config_path) as f:
+        net_cfg = json.load(f)['encoder']
+    encoder = GambleEncoder(
+        order_embedding_dim=net_cfg['order_embedding_dim'],
+        claim_embedding_dim=net_cfg['claim_embedding_dim'],
+        courier_embedding_dim=net_cfg['courier_embedding_dim'],
+        route_embedding_dim=net_cfg['route_embedding_dim'],
+        point_embedding_dim=net_cfg['point_embedding_dim'],
+        number_embedding_dim=net_cfg['number_embedding_dim'],
+        max_num_points_in_route=max_num_points_in_route,
+        device=None,
+    )
+    ac = DeliveryActorCritic(gamble_encoder=encoder, clm_emb_size=net_cfg['claim_embedding_dim'], device=None,
+                             temperature=1.0)
+    ac.load_state_dict(torch.load('checkpoints/6313c9d40bce480f8b1416a0f0976544.pt', map_location='cpu'))
+    dsp = NeuralSequantialDispatch(actor_critic=ac, max_num_points_in_route=max_num_points_in_route)
+    res = evaluate(
+        dispatch=dsp,
+        run_id=3,
+        simulator_cfg_path='configs/simulator.json',
+        sampler_mode='distr_sampler',
+        max_num_points_in_route=4,
+        history_db_path='history.db',
+        eval_num_simulator_steps=200,
+    )
+    print(res)
 
 
 def debug():
